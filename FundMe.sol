3d42d451d23ad2d7806a12d1c9ddae8922d65495
Gobi Ananth <gobiananthag@gmail.com>
2024-11-01 14:07:00 +0530

    Fund Me Solidity

---
+++ FundMe.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+
+    address public immutable i_owner;
+
+    constructor () {
+        i_owner = msg.sender;
+    }
+
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+    address[] public funders;
+    mapping (address => uint256) public addressToAmountFunded;
+
+    function fund () public payable {
+        require(msg.value.getConvertionRate() >= MINIMUM_USD, "Didn't send enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw () public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed!");
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == i_owner, "Sender is not owner!");
+        _;
+    }
+
+    receive () external payable {
+        fund();
+    }
+
+    fallback () external payable {
+        fund();
+    }
+}
